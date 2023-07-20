bf599b28822449e3bf606817a5d9a4d5dae15df9
Lucas Kalani Ross <lkross@Lucass-MacBook-Pro.local>
2023-07-20 07:08:35 -0700

    initial commit (made gui/logic with tkinter)

---
+++ template.py
@@ -0,0 +1,16 @@
+from maze import choose_maze, turn_left, turn_right, move_forward
+
+def solve_maze1():
+
+    move_forward()
+
+def solve_maze2():
+    pass
+
+def solve_maze3():
+    pass
+
+def main():
+    choose_maze(2)
+
+main()
