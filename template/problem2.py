0a16690158caa9146e0284f6a7eabd6aa7dfa18b
Lucas Kalani Ross <lkross@Lucass-MacBook-Pro.local>
2023-07-22 01:10:46 -0700

    made all logic functions private. added wall_in_front() function. created 3 template files for student to do work on. cloned all files into solution (solutions to each problem + challenge) and template (empty files for students to attempt work AKA files we give them)

---
+++ template/problem2.py
@@ -0,0 +1,17 @@
+from logic import Logic
+
+logic = Logic()
+logic.load_maze2()
+
+'''
+=== HELPFUL FUNCTIONS ===
+
+logic.turn_left()       Turns the player 90 degrees left.
+logic.turn_right()      Turns the player 90 degrees right.
+logic.move_forward()    Moves the player forward 1 space in their current direction.
+logic.wall_in_front()   Returns [True] if the player is facing a wall, [False] otherwise.
+
+=========================
+'''
+
+#Use this space to solve maze 2 :)
